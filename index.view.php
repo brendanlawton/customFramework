f1054bf11de75a6798bcf768170bfb300750ee89
Brendan Lawton <balawton87@gmail.com>
2018-01-11 12:47:06 +0000

    *Quick tidy up - line indentation - remove unused class

--- index.view.php
+++ index.view.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>My website</title>
+    <meta charset="UTF-8">
+    <title>My website</title>
 </head>
 <body>
-	<ul>
-		<?php foreach ($tasks as $task) : ?>
-			<?= ($task->completed) ? '<strike>' : ''; ?>
-				<li><?= $task->description; ?></li>
-			<?= ($task->completed) ? '</strike>' : ''; ?>
-		<?php endforeach; ?>
-	</ul>
+<ul>
+    <?php foreach ($tasks as $task) : ?>
+
+        <?= $task->completed ? '<strike>' : ''; ?>
+
+        <li><?= $task->description; ?></li>
+
+        <?= $task->completed ? '</strike>' : ''; ?>
+
+    <?php endforeach; ?>
+</ul>
 </body>
 </html>
