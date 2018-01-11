8e5d0b94f539803bb6ac75b78cf40b52f8f6c42d
Brendan Lawton <balawton87@gmail.com>
2018-01-11 10:48:47 +0000

    Initial commit

---
+++ index.view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My website</title>
+</head>
+<body>
+	<ul>
+		<?php foreach ($tasks as $task) : ?>
+			<?= ($task->completed) ? '<strike>' : ''; ?>
+				<li><?= $task->description; ?></li>
+			<?= ($task->completed) ? '</strike>' : ''; ?>
+		<?php endforeach; ?>
+	</ul>
+</body>
+</html>
