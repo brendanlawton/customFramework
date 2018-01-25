c05c2fbf19e9e631f9f6ac80dc0c2bf11b7b146b
Brendan Lawton <balawton87@gmail.com>
2018-01-25 13:08:10 +0000

    Split files/directories into app/core and MVC

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ require 'core/bootstrap.php';
 use App\Core\{Router, Request};
 
 // register and find the route associated with the requested uri
-Router::load('routes.php')
+Router::load('app/routes.php')
     ->direct(Request::uri(), Request::method());
